e0e460073e34c921a60d262e03122835fa813f06
playsoksana <playsoksana@gmail.com>
2021-02-12 17:50:49 +0200

    обьекты + массивы

--- js/object.js
+++ js/object.js
@@ -63,4 +63,43 @@ owedMoney["Джимми"] = owedMoney["Джимми"] + 3;
 owedMoney["Джимми"] +=   3;
 console.log(owedMoney["Джимми"]);
 
+// Хранение информации о фильмах
+let movies = {
+"В поисках Немо": {
+releaseDate: 2003,
+duration: 100,
+actors: ["Альберт Брукс", "Эллен Дедженерес",
+"Александр Гоулд"],
+format: "DVD"
+},
+"Звездные войны: Эпизод VI — Возвращение джедая": {
+releaseDate: 1983,
+duration: 134,
+actors: ["Марк Хэмилл", "Харрисон Форд", "Кэрри Фишер"],
+format: "DVD"
+},
+"Гарри Поттер и Кубок огня": {
+releaseDate: 2005,
+duration: 157,
+actors: ["Дэниел Рэдклифф", "Эмма Уотсон", "Руперт Гринт"],
+format: "Blu-ray"
+}
+};
 
+console.log(movies["В поисках Немо"]);
+
+console.log(movies["В поисках Немо"].format);
+
+
+
+
+let cars = {
+releaseDate: 2006,
+duration: 117,
+actors: ["Оуэн Уилсон", "Бонни Хант", "Пол Ньюман"],
+format: "Blu-ray"
+};
+
+movies["Тачки"] = cars;
+
+console.log(Object.keys(movies));
