13055cc2e27312a2e877821842eeaba3a530699e
playsoksana <playsoksana@gmail.com>
2021-03-30 09:13:49 +0300

    123

--- js/modul6.js
+++ js/modul6.js
@@ -923,22 +923,29 @@
 // console.log(getNamesSortedByFriendCount(arr));
 
 //TASK43
- //Пиши код ниже этой строки
-const getSortedFriends = users => users
-  .map(item => item.friends)
-  .flat(1).filter((item, i, arr) => arr.indexOf(item)=== i)
-  .sort((item, nextItem) => item.localeCompare(nextItem));
-//Пиши код выше этой строки
+//  //Пиши код ниже этой строки
+// const getSortedFriends = users => users
+//   .map(item => item.friends)
+//   .flat(1).filter((item, i, arr) => arr.indexOf(item)=== i)
+//   .sort((item, nextItem) => item.localeCompare(nextItem));
+// //Пиши код выше этой строки
 
 
-//TASK44
+// //TASK44
 
-// Пиши код ниже этой строки
-const getTotalBalanceByGender = (users, gender) => users
-.filter(item => item.gender === gender)
-.reduce((acc, item) => {
-  return acc += item.balance}, 0);
-// Пиши код выше этой строки
+// // Пиши код ниже этой строки
+// const getTotalBalanceByGender = (users, gender) => users
+// .filter(item => item.gender === gender)
+// .reduce((acc, item) => {
+//   return acc += item.balance}, 0);
+// // Пиши код выше этой строки
+
+// complete the function
+function solution(string) {
+    return string.split("").includes('c');
+}
+
+console.log(solution("camelCasing"));
 
 
 
